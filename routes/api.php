0f6b636434b971347ae49a209136f6498a255a89
Umaha Tokula <umahatokula@gmail.com>
2024-07-10 18:30:13 +0100

    Editedd api routes

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\SubscriptionController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::post('/posts', [PostController::class, 'store']);
 Route::post('/subscribe', [SubscriptionController::class, 'store']);
